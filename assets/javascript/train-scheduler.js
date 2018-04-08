aa30d5d505a2ef2f3dee105823ae5c16ddd3ebb7
gustavo2011 <gustavo.jauregui2011@gmail.com>
2018-04-08 11:33:55 -0700

    finished web layout

---
+++ assets/javascript/train-scheduler.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBWCyKBo33tehlJWB8qq_AeLpBD3kyFGgk",
+    authDomain: "train-scheduler-55bf9.firebaseapp.com",
+    databaseURL: "https://train-scheduler-55bf9.firebaseio.com",
+    projectId: "train-scheduler-55bf9",
+    storageBucket: "train-scheduler-55bf9.appspot.com",
+    messagingSenderId: "156644033305"
+  };
+  firebase.initializeApp(config);
